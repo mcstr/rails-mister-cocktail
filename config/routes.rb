4501062bd625c2b8ce741fdc78d7a79706257ab5
Miguel Castro <quecontentoestoy@gmail.com>
2020-04-12 16:53:35 +0200

    added controller, routes and seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: 'cocktail#index'
+resources :cocktails, only: [:index, :show, :create, :new] do
+  resources :doses, only: [:create, :edit, :update]
+end
+resources :doses, only: [:destroy]
 end
