8c2f592a92ea582e3f6a3d44082f2c7ed6aac3b1
Miguel Castro <quecontentoestoy@gmail.com>
2020-04-13 20:20:26 +0200

    added methods and views for cocktails and doses

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,44 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail, only: [:new, :create]
+  before_action :set_dose, only:[:destroy]
+  def index
+    @doses = Dose.all
+  end
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if
+    @dose.save
+    redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+  @dose.destroy
+  @cocktail = Cocktail.find(@dose[:cocktail_id])
+  redirect_to cocktail_path(@cocktail)
+  end
+
+
+  private
+
+  def set_dose
+    @dose = Dose.find(params[:dose_id])
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
+
